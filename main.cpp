79a88ab166bf07a0472d379211898a39f8979306
Djudax <Djudax122@gmail.com>
2024-03-15 11:56:49 +0200

    main2

--- main.cpp
+++ main.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
-#include <string>
-#include <vector>
+#include "Book.h"
 #include "Librarian.h"
 #include "Reader.h"
 
-using namespace std;
-
-
 int main() {
-    Book book1("Harry Poter", "J.K", 200);
+    // Створення об'єктів книг
+    Book book1("Harry Potter", "J.K", 200);
     Book book2("Financier", "T.D", 731);
 
-
+    // Створення об'єкта читача
     Reader reader("Anton", 18);
     reader.borrowBook(book1);
     reader.borrowBook(book2);
     reader.listBorrowedBooks();
 
+    // Створення об'єкта бібліотекаря
     Librarian librarian("Alisa");
     librarian.greet();
 
-
-
-
-
-
+    // Показ загальної кількості книг
+    Book::showTotalCount();
 
     return 0;
 }
