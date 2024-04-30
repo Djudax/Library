c84fb405a724d98b33ed313a37103b7a889d5e35
Djudax <Djudax122@gmail.com>
2024-04-30 11:51:26 +0300

    Lab5main

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include "book_reader.h"
 #include "book_librarian.h"
-#include <iostream>
 
 int main() {
-    // Створення об'єкту BookReader за допомогою конструктора
-    BookReader bookReader("The Great Gatsby", "F. Scott Fitzgerald", 1925, "Jane Doe");
-    // Виведення даних про книгу та читача
-    std::cout << "Book title borrowed by the reader: " << bookReader.getTitle() << std::endl;
-    std::cout << "Reader name: " << bookReader.getReaderName() << std::endl;
+    // Проблема статичної прив’язки методів (Static Method Binding)
+    Book book;
+    book.print(); // Виклик методу print з класу Book
 
-    // Створення об'єкту BookLibrarian за допомогою конструктора
-    BookLibrarian bookLibrarian("Pride and Prejudice", "Jane Austen", 1813, "Bob Smith");
-    // Виведення даних про книгу та бібліотекаря
-    std::cout << "Book title managed by the librarian: " << bookLibrarian.getTitle() << std::endl;
-    std::cout << "Librarian name: " << bookLibrarian.getLibrarianName() << std::endl;
+    // Використання динамічного поліморфізму з вказівником на базовий клас
+    BookReader bookReader;
+    BookLibrarian bookLibrarian;
+
+    Book* ptr = &bookReader;
+    ptr->print(); // Виклик віртуальної функції print з класу BookReader
+
+    ptr = &bookLibrarian;
+    ptr->print(); // Виклик віртуальної функції print з класу BookLibrarian
+
+    // Використання динамічного поліморфізму з посиланням на базовий клас
+    Reader& readerRef = bookReader;
+    readerRef.read(); // Виклик віртуальної функції read з класу BookReader
+
+    Librarian& librarianRef = bookLibrarian;
+    librarianRef.manage(); // Виклик віртуальної функції manage з класу BookLibrarian
 
     return 0;
 }
