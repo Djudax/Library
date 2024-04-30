5c784596b5337d508fc698c9c2e24ad0e3c262a3
Djudax <Djudax122@gmail.com>
2024-04-30 11:50:59 +0300

    book_reader5

--- book_reader.cpp
+++ book_reader.cpp
@@ -1,33 +1,14 @@
 #include "book_reader.h"
+#include <iostream>
 
-// Реалізація конструкторів
-BookReader::BookReader(const std::string& title, const std::string& author, int year, const std::string& readerName)
-        : Book(title, author, year), Reader(readerName) {}
-
-BookReader::BookReader(const BookReader& other)
-        : Book(other), Reader(other) {}
-
-BookReader::BookReader(BookReader&& other) noexcept
-        : Book(std::move(other)), Reader(std::move(other)) {}
-
-// Реалізація оператора присвоєння
-BookReader& BookReader::operator=(const BookReader& other) {
-    if (this != &other) {
-        static_cast<Book&>(*this) = other;
-        static_cast<Reader&>(*this) = other;
-    }
-    return *this;
+void BookReader::print() {
+    std::cout << "BookReader printed" << std::endl;
 }
 
-BookReader& BookReader::operator=(BookReader&& other) noexcept {
-    if (this != &other) {
-        static_cast<Book&>(*this) = std::move(other);
-        static_cast<Reader&>(*this) = std::move(other);
-    }
-    return *this;
+void BookReader::read() {
+    std::cout << "BookReader reading" << std::endl;
 }
 
-// Реалізація додаткового методу
-std::string BookReader::getReaderName() const {
-    return Reader::getName();
+BookReader::~BookReader() {
+    std::cout << "BookReader destroyed" << std::endl;
 }
